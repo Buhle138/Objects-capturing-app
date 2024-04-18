e2d68c5b04ea02b6eabc7d6a103233fb9f85a4b9
buhleradzilani <radzilanibuhle@gmail.com>
2024-04-18 11:13:33 +0200

    creating the user interface

---
+++ app/src/main/java/com/example/assignmentparttwo/GradientBox.kt
@@ -0,0 +1,28 @@
+package com.example.assignmentparttwo
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+
+
+val Blue = Color(0xFF0D4C92)
+val Tail600 = Color(0xFF59C180)
+
+@Composable
+fun GradientBox(
+    modifier: Modifier = Modifier,
+    content: @Composable BoxScope.() -> Unit
+){
+
+    Box(
+        modifier = modifier.background(brush = Brush.linearGradient(
+            listOf(Blue, Tail600)))
+    ){
+        content()
+    }
+
+}
